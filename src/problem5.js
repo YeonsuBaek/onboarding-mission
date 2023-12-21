07cef5405465bf1d463bc227c37f471508f2b331
Yeonsu Summer <soothbugger@gmail.com>
2023-12-22 08:21:14 +0900

    refactor: 코드 리팩터링

--- src/problem5.js
+++ src/problem5.js
@@ -1,43 +1,32 @@
 function problem5(money) {
-  const checkedError = checkError(money);
-
-  if (checkedError === "Not Error") {
-    const result = countByBillUnit(money);
-
-    return result;
-  }
-
-  return checkedError;
+  const isError= checkError(money);
+  return isError ? -1 : countByBillUnit(money);
 }
 
-function checkError(currentMoney) {
-  const MINIMUM_NUMBER = 1;
-  const MAXIMUN_NUMBER = 1000000;
-
-  if (!Number.isInteger(currentMoney)) {
-    return "money는 자연수이어야 합니다.";
-  }
-
-  if (currentMoney < MINIMUM_NUMBER || currentMoney > MAXIMUN_NUMBER) {
-    return "money는 1 이상 1,000,000 이하인 자연수이어야 합니다.";
-  }
-
-  return "Not Error";
+function checkError(money) {
+  return (isNumber(money) || isValidRange(money);
 }
 
-function countByBillUnit(currentMoney) {
+function isNumber(money) {
+  return !Number.isInteger(currentMoney);
+}
+
+function isValidRange(money) {
+    const MONEY = { MIN: 1, MAX: 1000000 };
+  return currentMoney < MONEY.MIN || currentMoney > MONEY.MAX;
+}
+
+function countByBillUnit(money) {
   const BILL_UNIT_LIST = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   const MINIMUM_BILL_NUMBER = 1;
-  const listLength = BILL_UNIT_LIST.length;
-  let billCountList = new Array(listLength).fill(0);
+  let billCountList = new Array(BILL_UNIT_LIST.length).fill(0);
 
   BILL_UNIT_LIST.forEach((bill, index) => {
-    const theNumbeOfBill = Math.floor(currentMoney / bill);
+    const billCount = Math.floor(money / bill);
 
-    if (theNumbeOfBill >= MINIMUM_BILL_NUMBER) {
-      billCountList[index] += theNumbeOfBill;
-
-      currentMoney -= theNumbeOfBill * bill;
+    if (billCount >= MINIMUM_BILL_NUMBER) {
+      billCountList[index] += billCount;
+      money -= billCount * bill;
     }
   });
 
